2fc2d608bd67a7cf11a7430f89195f70802c1043
Lê Quang Minh <106050463+minhlqBHsoft@users.noreply.github.com>
2022-06-21 09:21:45 +0700

    All the codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import About from "./Pages/About";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+import ErrorPage from "./Pages/ErrorPage";
+import Gallery from "./Pages/Gallery";
+import Home1 from "./Pages/Home1";
+import Home2 from "./Pages/Home2";
+import Service from "./Pages/Service";
+import Team from "./Pages/Team";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am your father
-        </a>
-      </header>
+const App = () => (
+    <div>
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home1/>}></Route>
+                <Route path='/home2' element={<Home2/>}></Route>
+                <Route path='/about' element={<About/>}></Route>
+                <Route path='/gallery' element={<Gallery/>}></Route>
+                <Route path='/service' element={<Service/>}></Route>
+                <Route path='/blog' element={<Blog/>}></Route>
+                <Route path='/contact' element={<Contact/>}></Route>
+                <Route path='/team' element={<Team/>}></Route>
+                <Route path="*" element={<ErrorPage/>}></Route>
+            </Routes>
+        </Router>
     </div>
-  );
-}
+);
 
 export default App;
